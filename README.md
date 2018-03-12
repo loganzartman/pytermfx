324767e050cd7d11a56b81d5da57cec21ec4a1e9
Logan Zartman <logan.zartman@utexas.edu>
2018-03-12 17:42:57 -0500

    Update setup, package data

---
+++ README.md
@@ -0,0 +1,5 @@
+# pytermfx
+### Terminal interaction and formatting for Python without curses
+---
+
+Installing: `pip install pytermfx`
--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@ from setuptools import setup
 setup(
 	name = 'pytermfx',
 	packages = ['pytermfx'],
-	version = '0.1',
-	description = '',
+	version = '0.1.0',
+	description = 'Terminal interaction and formatting for Python without curses',
 	author = 'Logan Zartman',
-	author_email = '',
+	author_email = 'logan.zartman@utexas.edu',
 	url = 'https://github.com/loganzartman/pytermfx',
-	download_url = 'https://github.com/loganzartman/pytermfx/archive/0.1.tar.gz',
-	keywords = [],
-	classifiers = [],
+	keywords = ['curses', 'terminal', 'colors', 'ansi', 'escapes'],
+	classifiers = [
+		'Development Status :: 3 - Alpha',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 3'
+	],
+	python_requires = ">=3"
 )
