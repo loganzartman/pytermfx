62352dc38e54c1633d556191627309c784990acc
Logan Zartman <logan.zartman@utexas.edu>
2018-03-13 01:52:07 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
 	name = 'pytermfx',
 	packages = ['pytermfx'],
-	version = '0.1.0',
+	version = '0.2.0',
 	description = 'Terminal interaction and formatting for Python without curses',
 	author = 'Logan Zartman',
 	author_email = 'logan.zartman@utexas.edu',
