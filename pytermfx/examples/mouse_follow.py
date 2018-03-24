df08c1aff8a8cae7fe2a998c960bfc74c85d522d
Logan Zartman <logan.zartman@utexas.edu>
2018-03-23 21:02:17 -0500

    Update mouse_follow example

--- pytermfx/examples/mouse_follow.py
+++ pytermfx/examples/mouse_follow.py
@@ -2,7 +2,8 @@ from pytermfx import Terminal, Color, Style
 from pytermfx.keys import MouseMove
 from time import clock
 
-SIZE = 16
+BASE_SIZE = 4
+MAX_SIZE = BASE_SIZE * 3
 old_x = 0
 old_y = 0
 
@@ -22,10 +23,24 @@ try:
 
 		c = t.getch()
 		if isinstance(c, MouseMove):
+			if c.down:
+				size = BASE_SIZE * 3
+			elif c.left or c.right:
+				size = BASE_SIZE * 2
+			else:
+				size = BASE_SIZE 
+
 			t.style(Style.none)
-			t.clear_box(old_x - SIZE // 2, old_y - SIZE // 4, SIZE, SIZE // 2)
-			t.style(Style("reverse"))
-			t.clear_box(c.x - SIZE // 2, c.y - SIZE // 4, SIZE, SIZE // 2)
+			t.clear_box(old_x - MAX_SIZE // 2, old_y - MAX_SIZE // 4, MAX_SIZE, MAX_SIZE // 2)
+			
+			if c.left:
+				t.style(Color.hex(0xFF0000).bg())
+			elif c.right:
+				t.style(Color.hex(0x00FF00).bg())
+			else:
+				t.style(Style("reverse"))
+			t.clear_box(c.x - size // 2, c.y - size // 4, size, size // 2)
+			
 			old_x = c.x
 			old_y = c.y
 			t.flush()
