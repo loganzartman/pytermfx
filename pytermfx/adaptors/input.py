be59133b46c7655420f76dcc9704ab193d81681b
nondefault <spam@nondefault.net>
2018-05-17 00:08:36 -0500

    Further improved accuracy of InputDaemon

--- pytermfx/adaptors/input.py
+++ pytermfx/adaptors/input.py
@@ -8,61 +8,79 @@ import queue
 class InputDaemon:
     def __init__(self, read_ch_func):
         self.read_ch = read_ch_func
-        self.grouper = Thread(target = self.group_main, daemon = True)
-        self.collector = Thread(target = self.collect_main, daemon = True)
 
-        self.input_event = Event()               # used to block until input
-        self.input_queue = Queue(maxsize = 256)  # input read by collector
-        self.buffer_timeout = 0.001              # group timeout in seconds
-        self.group_queue = []                    # groups of input from grouper
+        self.input_event = Event()              # used to block until input
+        self.input_queue = Queue(maxsize = 256) # input read by collector
+        self.buffer_timeout = 0.005             # group timeout in seconds
+        self.group_queue = []                   # groups of input from grouper
         self.running = False
+    
+        # Prepare threads
+        self.grouper = Thread(target=self.group_main, daemon=True)
+        self.collector = Thread(target=self.collect_main, daemon=True)
 
     def _start(self):
         self.running = True
         self.grouper.start()
-        self.collector.start()        
+        self.collector.start()
     
     def read(self):
         # start up daemons lazily
+        # this ensures that cbreak is set, etc. etc.
         if not self.running:
             self._start()
         
         self.input_event.wait() # block until some input is buffered
         if len(self.group_queue) == 0:
+            # sometimes we are notified erroneously
             return ""
 
         # poll input queue
         group = self.group_queue.pop(0)
         if len(self.group_queue) == 0:
+            # no more input for now
             self.input_event.clear()
         return group
     
     def collect_main(self):
+        # Collector thread body
         while self.running:
-            # read character
-            ch = self.read_ch()
-            self.input_queue.put(ch)
-    
+            # read character from stdin and enqueue
+            ch = self.read_ch()      # blocks if stdin empty
+            self.input_queue.put(ch) # blocks if queue full
+
     def group_main(self):
+        # Grouper thread body
         buffer = []
         grouping = True
+        timeout = 0
 
         def dump_group():
+            """Complete the current group of characters and dump to output.
+            """
+            nonlocal timeout
             if len(buffer) > 0:
+                timeout = 0
                 group = "".join(buffer)
                 buffer.clear()
                 self.group_queue.append(group)
-                self.input_event.set()
+                self.input_event.set()         # notify read() waiters
 
         while self.running:
             try:
-                timeout = self.buffer_timeout if grouping else self.buffer_timeout
+                # wait up to timeout seconds for next char in group
                 ch = self.input_queue.get(timeout = timeout)
                 if ch == chr(27):
+                    # we have read an ESC; start a new group
+                    # we can also immediately dump any previous group
                     dump_group()
+                    timeout = self.buffer_timeout * 10
                     grouping = True
-                buffer.append(ch)
+                if len(buffer) == 2 and ch != "M":
+                    timeout = self.buffer_timeout
+                buffer.append(ch) # append character to group
             except queue.Empty:
+                # timeout occurred; dump the group
                 dump_group()
                 grouping = False
 
